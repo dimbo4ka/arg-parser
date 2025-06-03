d0e8be3657627218f9317068a9abc20deda78a13
dimbo4ka <dimbo4kait@gmail.com>
2025-06-03 20:28:13 +0300

    feat(argument): implement Argument's main methods

--- src/Argument/Argument.cpp
+++ src/Argument/Argument.cpp
@@ -1,1 +1,34 @@
 #include "Argument.hpp"
+
+namespace ArgumentParser {
+template<typename T>
+Argument<T>::Argument() 
+        : stored_values_(nullptr), stored_value_(nullptr)
+        , min_args_(0)
+        , is_positional_(false), is_multivalue_(false) {}
+
+template<typename T>
+Argument<T>& Argument<T>::Default(T) {
+    default_value_ = std::move(T);
+}
+template<typename T>
+Argument<T>& Argument<T>::MultiValue(std::size_t min_args) {
+    is_multivalue_ = true;
+    min_args_ = min_args;
+}
+
+template<typename T>
+Argument<T>& Argument<T>::Positional() {
+    is_positional_ = true;
+}
+
+template<typename T>
+Argument<T>& Argument<T>::StoreValue(T& stored_value) {
+    stored_value_ = &stored_value;
+}
+
+template<typename T>
+Argument<T>& Argument<T>::StoreValues(std::vector<T>& stored_values) {
+    stored_values_ = &stored_values;
+}
+}
--- src/Argument/Argument.hpp
+++ src/Argument/Argument.hpp
@@ -10,8 +10,7 @@ template<typename T>
 class Argument : public BaseArgument {
 public:
     Argument();
-
-    Argument& Default(const T&);
+    Argument& Default(T);
     Argument& MultiValue(std::size_t = 0);
     Argument& Positional();
     Argument& StoreValue(T&);
@@ -21,6 +20,7 @@ private:
     std::vector<T>* stored_values_;
     T* stored_value_;
     std::size_t min_args_;
+    T default_value_;
     bool is_positional_;
     bool is_multivalue_;
 };
